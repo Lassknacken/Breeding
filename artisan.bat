f1239ef7ebcb27fa3e83e42f8a95fd1389aac742
Benjamin <zwergenkoenig_ks@web.de>
2016-11-26 14:54:28 +0100

    first add

---
+++ artisan.bat
@@ -0,0 +1,6 @@
+rem start iexplore.exe http://192.168.2.105:8000
+rem cd C:\Users\Benjamin\Documents\GitHub\Kostenverwaltung
+rem cd C:\Users\Benjamin\Documents\GitHub\survcam
+rem php artisan serv --host 192.168.178.26 --port 8000
+
+php -S localhost:8000
