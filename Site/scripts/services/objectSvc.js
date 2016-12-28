1c497f133c67f8ecc1c6b449e4dd0504b08bf278
Benjamin <zwergenkoenig_ks@web.de>
2016-12-28 14:54:34 +0100

    first dog from Database

--- Site/scripts/services/objectSvc.js
+++ Site/scripts/services/objectSvc.js
@@ -1,17 +1,40 @@
-services['objectService']=function() {
-    
-    this.createDog=function(data){
+ngServices['objectSvc']=function() {
+    var _self=this;
+
+
+    _self.createDogs=function(data){
+        if(!data && !data.dogs && !data.dogs.records){
+            return undefined;
+        }
+
+        var result=[];
+        for(var i=0; i<data.dogs.records.length;i++){
+            result.push(_self.createDog(data.dogs.records[i]));
+        }
+        
+        return result;
+    };
+
+    _self.createDog=function(data){
+
         if(!data){
             return undefined;
         }
 
         var result={};
-        result.id
 
-    };
+        result.id=data[0];
+        result.name=data[1];
+        result.birth=new Date(data[2]);
+        result.male=data[3];
+        result.chipnumber=data[4];
+        result.formvalue_id=data[5];
+
+        return result;
+    }
 
 //======
-    this.createMany=function(method,arr){
+    _self.createMany=function(method,arr){
         if(arr==undefined || arr.length===0){
             return [];
         }
