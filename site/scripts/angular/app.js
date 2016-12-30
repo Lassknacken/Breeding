903d74c134b8e187e8a7f74c1bfc2607b8c449e9
Benjamin <zwergenkoenig_ks@web.de>
2016-12-30 15:04:14 +0100

    -added dynamic list

--- site/scripts/angular/app.js
+++ site/scripts/angular/app.js
@@ -8,8 +8,12 @@ for(var key in ngControllers){
     myApp.controller(key, ngControllers[key]);
 }
 
-for(key in ngServices){
+for(var key in ngServices){
     myApp.service(key, ngServices[key]);
+}
+
+for(var key in ngDirectives){
+    myApp.directive(key, ngDirectives[key]);
 }
 
 myApp.config(function($routeProvider) {
@@ -23,7 +27,7 @@ myApp.config(function($routeProvider) {
     }).when("/edit_modus", {
         templateUrl : "/site/templates/edit_modus.htm"
     }).when("/dogs", {
-        templateUrl : "/site/templates/list.htm"
+        templateUrl : "/site/templates/table/list.htm"
     })
 });
 
