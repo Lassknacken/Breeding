d3e48658c9db80b98037b8b655122d4d3b5fadf3
Benjamin <zwergenkoenig_ks@web.de>
2017-01-08 15:28:29 +0100

    Automatism for startup

--- host.bat
+++ host.bat
@@ -3,4 +3,6 @@ rem cd C:\Users\Benjamin\Documents\GitHub\Kostenverwaltung
 rem cd C:\Users\Benjamin\Documents\GitHub\survcam
 rem php artisan serv --host 192.168.178.26 --port 8000
 
-php -S localhost:8000
+start "chrome" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://localhost:8000/Site/ --remote-debugging-port=9222 
+
+start "php" php -S localhost:8000
